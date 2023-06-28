2d720afa3e49676e91f9cd22b2a441220583b670
David Martinez <dmtz0016@gmail.com>
2023-06-27 18:38:04 -0600

    Added best suitable solution.

--- Javascript Course/Session snippets/Session 5/extra-challenge.js
+++ Javascript Course/Session snippets/Session 5/extra-challenge.js
@@ -23,7 +23,7 @@ function gradeProcessor(grades) {
                                  }
     })
 
-//Without using indexes
+//Without using indexes (wrong way)
     grades.forEach((grade) => {
         if(grade >= 90 && grade <= 100)
         {
@@ -44,8 +44,32 @@ function gradeProcessor(grades) {
                                 console.log(grade + " Outside scope")
                                  }
     })
-  }
+ 
   
+
+//Without using indexes (best way)
+ 
+    for(grade of grades){
+        if(grade >= 90 && grade <= 100)
+        {
+            console.log("Grade: " + grade + " is A")
+        }else if(grade >= 80 && grade < 90)
+            {
+                console.log("Grade: " + grade + " is B")
+            }else if(grade >= 70 && grade < 80)
+                {  
+                    console.log("Grade: " + grade + " is C")
+                }else if(grade >= 60 && grade < 70)
+                    {
+                        console.log("Grade: " + grade + " is D")
+                    }else if(grade < 60 && grade >= 0)
+                            {
+                                console.log("Grade: " + grade + " is F")    
+                            }else{
+                                console.log(grade + " Outside scope")
+                                 }
+    }
+}
   // Ejemplo
 gradeProcessor([90, 95, 80, 96, 76, 74, 102, 86, 100, 67, 40, 88, 69, 70, -1, 98])
 
